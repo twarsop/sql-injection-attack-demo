acb7bc05b9d27e9acf008cb38b508203585cf4a7
t_warsop <tewarsop@gmail.com>
2020-10-21 11:04:38 +0100

    added customer table creation to create script and example insert

--- Database/create.sql
+++ Database/create.sql
@@ -13,3 +13,16 @@ VALUES (1, 'Mr'),
 (4, 'Dr'),
 (5, 'Ms'),
 (6, 'Prof');
+
+CREATE TABLE sqlinjectionattackdemo.public.customers
+(
+    Id SERIAL NOT NULL PRIMARY KEY,
+    TitleId INT NOT NULL REFERENCES sqlinjectionattackdemo.public.titles(Id),
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    AddressLine1 VARCHAR(255) NOT NULL,
+    AddressPostcode VARCHAR(255) NOT NULL
+);
+
+INSERT INTO sqlinjectionattackdemo.public.customers (TitleId, FirstName, LastName, AddressLine1, AddressPostcode)
+VALUES (1, 'FirstName', 'LastName', 'AddressLine1', 'AddressPostcode');
