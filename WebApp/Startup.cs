af7bdaa3fed26dfaf735d1343c000a8c8f37ed68
t_warsop <tewarsop@gmail.com>
2020-10-27 21:03:35 +0000

    renamed Home controller to Customer

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -35,7 +35,7 @@ namespace WebApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Customer/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Customer}/{action=Index}/{id?}");
             });
         }
     }
