0afc7af3e4f8fc101464fb6a015a9c7eae3649d2
t_warsop <tewarsop@gmail.com>
2020-10-30 17:03:12 +0000

    because we were able to swap the new ef models for the original (using data annotations) removed the now redundant creation of the customer list created by the GetAll method

--- DataLayer/DataLayer/Repositories/CustomerRepositoryEF.cs
+++ DataLayer/DataLayer/Repositories/CustomerRepositoryEF.cs
@@ -22,35 +22,12 @@ namespace DataLayer.Repositories
 
         public List<Customer> GetAll()
         {
-            List<Title> titles = _titleRepository.GetAll();
-            var titleLookup = new Dictionary<int, Title>();
-            foreach (Title title in titles)
-            {
-                titleLookup.Add(title.Id, title);
-            }
-
-            var customers = new List<Customer>();
-            
             var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
             optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=postgres;Database=sqlinjectionattackdemo");
             using (var context = new MyDbContext(optionsBuilder.Options))
             {
-                var customersEF = context.Customers.Include(c => c.Title).ToList();
-                foreach (DataLayer.Models.Customer customer in customersEF)
-                {
-                    customers.Add(new Customer
-                    {
-                        Id = customer.Id,
-                        Title = new Title{ Id = customer.Title.Id, Name = customer.Title.Name },
-                        FirstName = customer.FirstName,
-                        LastName = customer.LastName,
-                        AddressLine1 = customer.AddressLine1,
-                        AddressPostcode = customer.AddressPostcode
-                    });
-                }
+                return context.Customers.Include(c => c.Title).ToList();
             }
-
-            return customers;
         }
 
         public List<Customer> Search(Customer c)
